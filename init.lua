5349410e38179f92a41608f6c51c2ec7ac476d61
Cooper LeBrun <cooperlebrun@gmail.com>
2023-11-18 14:47:00 -0500

    add nvim-surround plugin

--- init.lua
+++ init.lua
@@ -129,6 +129,17 @@ require('lazy').setup({
   -- },
 
   {
+    "kylechui/nvim-surround",
+    version = "*", -- Use for stability; omit to use `main` branch for the latest features
+    event = "VeryLazy",
+    config = function()
+      require("nvim-surround").setup({
+        -- Configuration here, or leave empty to use defaults
+      })
+    end
+  },
+
+  {
     'folke/tokyonight.nvim',
     config = function()
       vim.cmd.colorscheme 'tokyonight'
