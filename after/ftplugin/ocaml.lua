63f76cfc4a3f62f7cdc94f448e5a440939f3e4f8
Cooper LeBrun <cooperlebrun@gmail.com>
2023-12-04 22:48:20 -0500

    factor out tmux popup bind function

--- after/ftplugin/ocaml.lua
+++ after/ftplugin/ocaml.lua
@@ -1,39 +1,33 @@
 vim.opt_local.shiftwidth = 2
 
--- dune build in tmux popup
-vim.keymap.set('n', '<localleader>db',
-	":silent! ! tmux popup -h 90\\% -w 80\\% 'dune build'<CR>",
-	-- tmux popups don't inherit some env vars when running a command,
-	-- so to get dune to work we need to make sure the env for opam is
-	-- set correctly.
-	-- ":silent! ! tmux popup -h 90\\% -w 80\\% 'eval $(opam env --set-switch) && dune build'<CR>",
-	{ buffer = true, desc = '[d]une [b]uild' })
+local function def_popup_command(bind, com_str, options)
+	local options = options or {}
+	local height = options.h or '90'
+	local width = options.h or '80'
+	local command = string.format(
+	-- zsh -ic ensures any env stuff you set up in your .zshrc or whatever
+	-- is actually set inside a non-interactive popup
+	-- \\ + %% = escaping for % inside a string.format that's sent to :!
+		":silent! ! tmux popup -h %s\\%% -w %s\\%% 'zsh -ic \"%s\"'<CR>",
+		height, width, com_str
+	)
+
+	local desc = options.desc or com_str
+	vim.keymap.set('n', bind, command, { buffer = true, desc = desc })
+end
+
+def_popup_command("<localleader>db", "dune build", { desc = "[d]une [b]uild" })
+def_popup_command("<localleader>dR", "dune build --profile release", { desc = "[d]une [R]elease build" })
+def_popup_command("<localleader>dt", "dune test", { desc = "[d]une [t]est" })
+def_popup_command("<localleader>dT", "OCAMLPARAM='_,warn-error=-a' dune test",
+	{ desc = "[d]une [T]est (ignore warnings)" })
+def_popup_command("<localleader>du", "OCAMLPARAM='_,warn-error=-a' dune utop",
+	{ desc = "[d]une [u]top (ignore warnings)" })
 
 -- dune build and send output to buffer
 vim.keymap.set('n', '<localleader>dB',
 	":r! dune build<CR>",
 	{ buffer = true, desc = '[d]une [B]uild (send output to buffer)' })
-
--- dune relase build in tmux popup
--- Nice because it doesn't error when there are warnings
-vim.keymap.set('n', '<localleader>dR',
-	":silent! ! tmux popup -h 90\\% -w 80\\% 'dune build --profile release'<CR>",
-	{ buffer = true, desc = '[d]une [R]elease build' })
-
--- dune test
-vim.keymap.set('n', '<localleader>dt',
-	":silent! ! tmux popup -h 90\\% -w 80\\% 'dune test'<CR>",
-	{ buffer = true, desc = '[d]une [t]est' })
-
--- dune test, suppressing warnings
-vim.keymap.set('n', '<localleader>dT',
-	":silent! ! tmux popup -h 90\\% -w 80\\% \"OCAMLPARAM='_,warn-error=-a' dune test\"<CR>",
-	{ buffer = true, desc = '[d]une [T]est (suppress warnings)' })
-
--- dune utop
-vim.keymap.set('n', '<localleader>du',
-	":silent! ! tmux popup -h 90\\% -w 80\\% \"OCAMLPARAM='_,warn-error=-a' dune utop\"<CR>",
-	{ buffer = true, desc = '[d]une [u]top (suppress warnings)' })
 
 -- dune promote
 -- when you are using ppx_expect and you run dune test, it shows a diff of the updated
